7d36103d3854a98237b7aa0a2272fdb5bffe6f1a
Serge Ivo <serge.the.dev@gmail.com>
2026-03-05 16:10:45 +1100

    Simplify mallguide tmux session to code and shell windows

--- scripts/tmux/mallguide.sh
+++ scripts/tmux/mallguide.sh
@@ -22,16 +22,5 @@ tmux send-keys -t "$session:code" "cd \"$root\"" C-m
 tmux new-window -t "$session" -n "shell" -c "$root"
 tmux send-keys -t "$session:shell" "cd \"$root\"" C-m
 
-# Window 3: local-dev
-
-tmux new-window -t "$session" -n "local-dev" -c "$root"
-tmux split-window -v -t "$session:local-dev" -c "$root"
-
-# Pane 0: website
-tmux send-keys -t "$session:local-dev.0" "cd \"$root\" && pnpm -C web dev --port 3013" C-m
-
-# Pane 1: admin website
-tmux send-keys -t "$session:local-dev.1" "cd \"$root\" && pnpm -C admin dev --port 3014" C-m
-
 tmux select-window -t "$session:code"
 echo "$log_prefix ready"
